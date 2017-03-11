d55bb3847235b3ffda19759c33d70c0b1a47367c
dangnabit <dan.gabel@gmail.com>
2017-03-11 13:17:51 -0600

    App update

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
 
 
 
-
     /* TODO 
 
     	
@@ -18,23 +17,25 @@ $(document).ready(function() {
 		sanitize year input
 
     */
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    
-    url += '?' + $.param({
-        'api-key': "9707b10b5ab44dada00194c572da1017",
-        'q': $searchQuery.value,
-        'begin_date': $startYear.value,
-        'end_date': $endYear.value
-    });
-    
+    searchBtn.on('click', function() {
 
-    $.ajax({
-        url: url,
-        method: 'GET',
-    }).done(function(result) {
-        console.log(result);
-    });
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
+        url += '?' + $.param({
+            'api-key': "9707b10b5ab44dada00194c572da1017",
+            'q': $searchQuery.value,
+            'begin_date': $startYear.value,
+            'end_date': $endYear.value
+        });
+
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function(result) {
+            console.log(result);
+        });
+    });
 
 
 
